6ea77ca9281ecc4f4ddcacb1423d1bb145b524b9
DhenryKamwela <51858426+DhenryKamwela@users.noreply.github.com>
2019-12-05 18:04:38 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,25 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab31050
+namespace _3Lab1050
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 10;
-            while (i <= 20)
+            int i = 1;
+            while (i <= 10)
             {
                 Console.WriteLine(i);
                 i += 1;
-
             }
+
             Console.ReadKey();
 
 
 
 
 
+
+
+
+
+
+
+
         }
+
+
+
+
+
+
     }
 }
