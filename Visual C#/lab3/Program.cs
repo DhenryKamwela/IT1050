9a12161f4d07c1779f4f80b4b7b5da7a09288d1e
DhenryKamwela <51858426+DhenryKamwela@users.noreply.github.com>
2019-12-05 18:01:45 -0500

    Add files via upload

---
+++ Visual C#/lab3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if statement - Write a program that converts a Fahrenheit temperature to Celsius.
+            //The user should be able to input the temperature at the Command prompt and it should output the temperature.
+            //In addition, you should output “It is cold” if the Fahrenheit value is less that 40 or it should output 
+            //“It is hot” if the temperature is over 90 (4 points).
+            //Code to read a value: double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            //Code to convert: celsius  = (fahrenheit - 32d) * 5d / 9d;
+
+            double a = 40;
+            double b = 90;
+
+            Console.WriteLine("Enter your temperature in degrees Fahrenheit.");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheit - 32d) * 5d / 9d;
+
+            if (fahrenheit > b)
+            {
+                Console.WriteLine("The tempurature in degrees Celsius is: " + celsius);
+                Console.WriteLine("It is hot");
+
+            }
+            else if (fahrenheit > a)
+            {
+                Console.WriteLine("The tempurature in degrees Celsius is: " + celsius);
+            }
+            else
+            {
+                Console.WriteLine("The tempurature in degrees Celsius is: " + celsius);
+                Console.WriteLine("It is cold");
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
