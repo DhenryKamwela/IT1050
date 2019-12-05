3677285b173011c892ed32ba310bb8ffe12ba47c
DhenryKamwela <51858426+DhenryKamwela@users.noreply.github.com>
2019-12-05 18:02:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab31050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            while (i <= 20)
+            {
+                Console.WriteLine(i);
+                i += 1;
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
